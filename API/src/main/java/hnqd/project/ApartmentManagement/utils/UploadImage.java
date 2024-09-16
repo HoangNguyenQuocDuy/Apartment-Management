594f80defa76cccdac936d3e676e6babb919a6f9
HoangNguyenQuocDuy64 <duy.hoang@affina.com.vn>
2024-09-16 15:15:21 +0700

    feat(utils): add UploadImage to cloudinary

--- API/src/main/java/hnqd/project/ApartmentManagement/utils/UploadImage.java
+++ API/src/main/java/hnqd/project/ApartmentManagement/utils/UploadImage.java
@@ -1,4 +1,23 @@
 package hnqd.project.ApartmentManagement.utils;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Component
 public class UploadImage {
+
+    @Autowired
+    private Cloudinary cloudinary;
+
+    public String uploadToCloudinary(MultipartFile file) throws IOException {
+        Map res = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap("folder", "quanlychungcu"));
+
+        return res.get("secure_url").toString();
+    }
 }
