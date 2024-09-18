8085ead79fb6fdb192ebe85164f1be02ced35d8a
HoangNguyenQuocDuy64 <duy.hoang@affina.com.vn>
2024-09-18 10:31:53 +0700

    feat(controller): add NotifyController to send notify from ADMIN

--- API/src/main/java/hnqd/project/ApartmentManagement/controller/NotifyController.java
+++ API/src/main/java/hnqd/project/ApartmentManagement/controller/NotifyController.java
@@ -1,4 +1,15 @@
 package hnqd.project.ApartmentManagement.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class NotifyController {
+
+    @MessageMapping("/ws")
+    @SendTo("/topic/notify")
+    public String send(String notify) {
+        return notify;
+    }
 }
