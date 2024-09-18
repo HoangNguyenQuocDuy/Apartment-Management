010ebd549b0ea86980c075c6f52c574bb962e50e
HoangNguyenQuocDuy64 <duy.hoang@affina.com.vn>
2024-09-18 10:39:21 +0700

    feat(api): add scheduled task to create room rental invoices via cron job

--- API/src/main/java/hnqd/project/ApartmentManagement/config/ApplicationConfig.java
+++ API/src/main/java/hnqd/project/ApartmentManagement/config/ApplicationConfig.java
@@ -1,11 +1,17 @@
 package hnqd.project.ApartmentManagement.config;
 
 import com.cloudinary.Cloudinary;
+import hnqd.project.ApartmentManagement.dto.InvoiceRequest;
+import hnqd.project.ApartmentManagement.entity.Room;
 import hnqd.project.ApartmentManagement.repository.IUserRepo;
+import hnqd.project.ApartmentManagement.service.IInvoiceService;
+import hnqd.project.ApartmentManagement.service.IRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -16,9 +22,11 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Configuration
+@EnableScheduling
 public class ApplicationConfig {
     @Value("${cloudinary.cloud-name}")
     private String cloudName;
@@ -28,15 +36,22 @@ public class ApplicationConfig {
     private String apiSecret;
 
     @Autowired
+    private IRoomService roomService;
+    @Autowired
     private IUserRepo userRepo;
+    @Autowired
+    private IInvoiceService invoiceService;
+
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> (UserDetails) userRepo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found!"));
     }
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
@@ -44,6 +59,7 @@ public class ApplicationConfig {
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
     }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
@@ -58,5 +74,23 @@ public class ApplicationConfig {
         config.put("secure", true);
 
         return new Cloudinary(config);
+    }
+
+    @Scheduled(cron = "0 10 15 17 * ?")
+    public void createInvoices() {
+        Map<String, String> paramsRoom = new HashMap<>();
+        paramsRoom.put("status", "Rented");
+        List<Room> rooms = roomService.getRooms(paramsRoom);
+
+        rooms.forEach(room -> {
+            InvoiceRequest invoice = InvoiceRequest.builder()
+                    .invoiceTypeId(3)
+                    .amount(room.getRoomType().getPrice())
+                    .roomId(room.getId())
+                    .description("Invoice rent")
+                    .build();
+
+            invoiceService.createInvoice(invoice);
+        });
     }
 }
